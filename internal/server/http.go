1b57c199e496bf29ee99f144fe65d1bfd9274d7f
Omar Adrian <oadrian38@gmail.com>
2025-02-07 17:11:53 -0600

    Implement interfaces to handle errors and how information is sended to client

--- internal/server/http.go
+++ internal/server/http.go
@@ -16,10 +16,17 @@ import (
 *SERIALIZACION DE DATOS
 */
 type ActivityDocument struct { //Esto es para serializar los datos
-	Activity Activity `json:"Activity"`
+	Activity 	Activity	`json:"Activity"`
 }
 type ResponseIDDocument struct { //Esto tambien es para serializar los datos. Este funciona solo para devolver un dato.
-	ID uint64 `json:"id"` //Este unicamente funciona para devolver un mensaje como respuesta del documento que se ha agregado
+	ID 		uint64 	`json:"id"` //Este unicamente funciona para devolver un mensaje como respuesta del documento que se ha agregado
+}
+type ResponseError struct {
+	Error 		string	`json:"error"`
+}
+
+type JSONResponse interface {
+	LaunchResponse(writer http.ResponseWriter, statusCode int)
 }
 
 /*
@@ -62,7 +69,6 @@ func renderHome(w http.ResponseWriter, r *http.Request) {
 	* addActivity/{id}
 */
 func (s *ActivitiesHandler) addActivity(w http.ResponseWriter, r *http.Request) {
-
 	if r.Method != http.MethodPost {
 		http.Error(w, "Metodo no permitido en esta ruta", http.StatusMethodNotAllowed)
 		return
@@ -75,50 +81,57 @@ func (s *ActivitiesHandler) addActivity(w http.ResponseWriter, r *http.Request) 
 		http.Error(w, "Bad request. Verify your data.", http.StatusInternalServerError)
 		//4. Recordar que la manera de manejar errores en las solicitudes es con http.Error
 		return
-	}else{
-		inserted := s.Activities.InsertActivity(request.Activity) //5. Simulacion del manejo de persistencia. Esto basado en activities
-		response := ResponseIDDocument{ID: inserted} //6. Generar una respuesta. Aqui si debemos de usar el formato de los struct json
-		w.Header().Set("Content-Type", "application/json") //7. Asignar header ya que estamos devolviendo formato json a la respuesta
-		encodedResponseOK := json.NewEncoder(w).Encode(response) //8. Codificar los datos y enviarlos en el Writer.
+	}
+	inserted := s.Activities.InsertActivity(request.Activity) //5. Simulacion del manejo de persistencia. Esto basado en activities
+	response := ResponseIDDocument{ID: inserted} //6. Generar una respuesta. Aqui si debemos de usar el formato de los struct json
+	w.Header().Set("Content-Type", "application/json") //7. Asignar header ya que estamos devolviendo formato json a la respuesta
+	w.WriteHeader(http.StatusCreated)
+	encodedResponseOK := json.NewEncoder(w).Encode(response) //8. Codificar los datos y enviarlos en el Writer.
 
-		if encodedResponseOK != nil { //9. Verificar que se haya hecho la conversion al enviar los datos de manera correcta
-			http.Error(w, "En caso de que no se haya convertido correctamente", http.StatusBadRequest)
-			return
-		}
-		fmt.Printf("Inserted activity was: %s with the id of %d\n", request.Activity.Title, inserted)
-	}
-
-}
-
-func (s *ActivitiesHandler) getActivity(w http.ResponseWriter, r *http.Request) {	
-	if r.Method != http.MethodGet { //Esto en caso de que el metodo usado no sea el correcto
-		http.Error(w, "Metodo no permitido en esta ruta", http.StatusMethodNotAllowed)
-		return
-	}
-	idStr := r.URL.Query().Get("id")
-	if idStr == "" { //Obtencion del parametro id del query
-		http.Error(w, "Se requiere un parametro 'id'", http.StatusBadRequest)
-		return
-	}
-	id, err := strconv.ParseUint(idStr, 10, 64) //Parseo del query param
-    if err != nil {
-        http.Error(w, "ID inválido", http.StatusBadRequest)
-        return
-    }
-	activityItem, err := s.Activities.GetActivity(id)
-	if err != nil { //Ejecucion de la funcion para agregar la actividad
-		http.Error(w, "Cannot retreat the item", http.StatusInternalServerError)
-		return
-	}
-	serializedResponse := ActivityDocument{Activity: activityItem} //Serializar en base al objeto de Activity
-	encodedResponseOK := json.NewEncoder(w).Encode(serializedResponse) //Encode y envio por ResposeWriter
 	if encodedResponseOK != nil { //9. Verificar que se haya hecho la conversion al enviar los datos de manera correcta
 		http.Error(w, "En caso de que no se haya convertido correctamente", http.StatusBadRequest)
 		return
 	}
+	// fmt.Printf("Inserted activity was: %s with the id of %d\n", request.Activity.Title, inserted)
+}
+
+func (s *ActivitiesHandler) getActivity(w http.ResponseWriter, r *http.Request) {	
+
+	var response JSONResponse
+
+	if r.Method != http.MethodGet { //Esto en caso de que el metodo usado no sea el correcto
+		response = ResponseError{Error: "Metodo no permitido en esta ruta"}
+		response.LaunchResponse(w, http.StatusMethodNotAllowed)
+		return
+	}
+	
+	idStr := r.URL.Query().Get("id")
+	if idStr == "" { //Obtencion del parametro id del query
+		response = ResponseError{Error: "Cannot retrieve id from query"}
+		response.LaunchResponse(w, http.StatusBadRequest)
+		return
+	}
+	id, err := strconv.ParseUint(idStr, 10, 64) //Parseo del query param
+	if err != nil {
+		response = ResponseError{Error: "Given id could not be parsed, try integer value"}
+		response.LaunchResponse(w, http.StatusBadRequest)
+		return
+	}
+	activityItem, err := s.Activities.GetActivity(id)
+	if err != nil { //Ejecucion de la funcion para agregar la actividad
+		response = ResponseError{Error: "Activity could not be found"}
+		response.LaunchResponse(w, http.StatusBadRequest)
+		return
+	}
+
+	response = ActivityDocument{Activity: activityItem}
+	response.LaunchResponse(w, http.StatusOK)
 }
 
 func (s *ActivitiesHandler) editActivity(w http.ResponseWriter, r *http.Request){
+
+	var response JSONResponse
+
 	if r.Method != http.MethodPatch {
 		http.Error(w, "Unable to use this route because verb", http.StatusMethodNotAllowed)
 		return
@@ -127,7 +140,8 @@ func (s *ActivitiesHandler) editActivity(w http.ResponseWriter, r *http.Request)
 	var newData ActivityDocument
 	unmarshalErr := json.NewDecoder(r.Body).Decode(&newData)
 	if unmarshalErr != nil {
-		http.Error(w, "Cannot convert data", http.StatusInternalServerError)
+		response = ResponseError{Error:"Cannot convert data"}
+		response.LaunchResponse(w, http.StatusInternalServerError)
 		return
 	}
 
@@ -139,25 +153,49 @@ func (s *ActivitiesHandler) editActivity(w http.ResponseWriter, r *http.Request)
 		}
 	}
 	if foundActivity == false {
-		http.Error(w, "Given id could not be found", http.StatusConflict)
+		fmt.Println("ehhlo")
+		response = ResponseError{Error:"Given id could not be found"}
+		response.LaunchResponse(w, )
 		return
 	}
 
 	//Manage to give a json response
 	actId, ok := s.Activities.EditActivity(newData.Activity)
 	if ok != nil {
-		http.Error(w, "Edit activity could not be applied. Maybe data does not exist", http.StatusConflict)
+		response = ResponseError{Error: "Edit activity could not be applied. Maybe data does not exist"}
+		response.LaunchResponse(w, http.StatusOK)
 		return
 	}
-	response := &ResponseIDDocument{
+	response = ResponseIDDocument{
 		ID: actId,
 	}
-	resErr := json.NewEncoder(w).Encode(response)
-	if resErr != nil {
-		http.Error(w, "Cannot procesate request", http.StatusConflict)
-	}
+	response.LaunchResponse(w, http.StatusOK)
 }
 
+
+/*
+	* Implementations for LaunchResponse
+*/
+func (r ResponseError) LaunchResponse(writer http.ResponseWriter, statusCode int) {
+	badResponse := ResponseError{Error:r.Error}
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(statusCode)
+	json.NewEncoder(writer).Encode(badResponse)
+}
+
+func (r ResponseIDDocument) LaunchResponse(writer http.ResponseWriter, statusCode int) {
+	response := ResponseIDDocument{ID: r.ID}
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(statusCode)
+	json.NewEncoder(writer).Encode(response)
+}
+
+func (r ActivityDocument) LaunchResponse(writer http.ResponseWriter, statusCode int) {
+	response := ActivityDocument{Activity: r.Activity}
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(statusCode)
+	json.NewEncoder(writer).Encode(response)
+}
 
 
 
